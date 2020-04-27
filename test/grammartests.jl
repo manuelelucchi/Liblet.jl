03c198554b139c9b7a1ec8131ec0bbc2c75d4335
Manuele Lucchi <>
2020-04-27 23:07:30 +0200

    implemented basic testing. Added some dependencies

---
+++ test/grammartests.jl
@@ -0,0 +1,42 @@
+using Test
+using Liblet
+
+test_production_wrong_lhs() = Production(1, ["a"])
+
+test_production_nonempty_lhs() = Production("",[""])
+
+test_production_wrong_rhs() = Production("a",[1])
+
+test_production_nonemptystr_rhs() = Production("a"["a","","c"])
+
+test_production_nonempty_rhs() = Production("a",[])
+
+test_production_such_that_lhs() = suchthat(left = "X")(Production('X', ['x']))
+
+function test_grammar_restrict_to()
+    G = Grammar("""
+        Z -> E
+        E -> T | E + T | X
+        X -> x
+        T -> i | ( E )
+    """)
+    Gr = Grammar("""
+        Z -> E
+        E -> T | E + T
+        T -> i | ( E )
+    """)
+    symbols = G.T ∪ G.N - Set(["X", "x"])
+    restrict(G, symbols) == Gr
+end
+
+
+@testset "production_tests" begin
+    @test_throws ArgumentError test_production_wrong_lhs()
+    #@test_throws ArgumentError test_production_nonempty_lhs()
+    #@test_throws ArgumentError test_production_wrong_rhs()
+    #@test_throws ArgumentError test_production_nonemptystr_rhs()
+    #@test_throws ArgumentError test_production_nonempty_rhs()
+
+    @test test_grammar_restrict_to()
+    @test test_production_such_that_lhs()
+end
